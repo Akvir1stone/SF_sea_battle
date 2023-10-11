81a573c5d3f761dbd0bea1e28767158c3070bc56
Akvir1stone <minnikaiev@mail.ru>
2023-10-11 13:53:02 +0300

    1

--- Classes.py
+++ Classes.py
@@ -22,3 +22,50 @@ class Ship:
             else:
                 dots.append(Dot(self.d.x, self.d.y+i))
         return dots
+
+    def countrs(self):
+        countrs = []
+        for i in range(3):
+            for j in range(self.size+2):
+                if self.hor:
+                    countrs.append(Dot(self.d.x+i-1, self.d.y+i-1))
+                else:
+                    countrs.append(Dot(self.d.x+i-1, self.d.y+i-1))
+        return countrs
+
+    def find_ship(self, d):
+        if d in self.dots():
+            return True
+        return False
+
+
+class Board:
+    def __init__(self):
+        # 2d list 6x6
+        self.board = [['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '']]
+        for i in range(6):
+            for j in range(6):
+                self.board[i][j] = Dot(i, j)
+        self.ships = []
+        self.countrs = []
+        self.ship_dots = []
+        self.destr_ship_dots = []
+        self.hiden = False
+
+    def add_ship(self, sh: Ship):
+        if sh.dots in self.board and sh.dots not in self.countrs:
+            self.ships.append(sh)
+            self.ship_dots += sh.dots()
+            self.countrs += sh.countrs()
+
+    def shoot(self, d: Dot):
+        if d not in self.board:
+            return 'out'
+        if d in self.ship_dots:
+            self.destr_ship_dots.append(self.ship_dots.pop(self.ship_dots.index(d)))
+            for i in self.ships:
+                if i.find_ship(d):
+                    i.hp -= 1
+                    if i.hp == 0:
+                        self.ships.pop(i)
+
